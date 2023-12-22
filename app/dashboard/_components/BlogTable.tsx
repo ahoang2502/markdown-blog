354e563cd00705d4675db7421b6b7941b51b91cb
ahoang2502 <athoang252@gmail.com>
2023-12-22 18:11:32 +1100

    blog list

--- app/dashboard/_components/BlogTable.tsx
+++ app/dashboard/_components/BlogTable.tsx
@@ -3,24 +3,30 @@ import React from "react";
 
 import { Switch } from "@/components/ui/switch";
 import { Button } from "@/components/ui/button";
+import { readBlog } from "@/lib/actions/blog";
 
-const BlogTable = () => {
+const BlogTable = async () => {
+	const { data: blogs } = await readBlog();
+
 	return (
 		<div className="overflow-x-auto">
 			<div className="bg-neutral-100 rounded-md w-[900px] md:w-full shadow-lg">
-				<div className="grid grid-cols-5 p-5 text-black font-medium border-b-2 border-b-lime-500/60 ">
+				<div className="grid grid-cols-5 p-5 text-black font-semibold border-b-2 border-b-lime-500/60 ">
 					<h1 className="col-span-2">Title</h1>
 					<h1 className="">Premium</h1>
 					<h1 className="">Publish</h1>
 				</div>
 
-				<div className="grid grid-cols-5 p-5">
-					<h1 className="col-span-2">Blog title</h1>
-					<Switch checked={false} />
-					<Switch checked={true} />
+				{/* blog-list */}
+				{blogs?.map((blog, index) => (
+					<div className="grid grid-cols-5 p-5" key={index}>
+						<h1 className="col-span-2 font-medium">{blog.title}</h1>
+						<Switch checked={blog.is_premium} />
+						<Switch checked={blog.is_published} />
 
-					<Actions />
-				</div>
+						<Actions />
+					</div>
+				))}
 			</div>
 		</div>
 	);
@@ -28,15 +34,15 @@ const BlogTable = () => {
 
 const Actions = () => (
 	<div className="flex items-center gap-2 flex-wrap md:flex-row">
-		<Button className="flex items-center gap-2">
+		<Button className="flex items-center gap-2" variant="outline">
 			<EyeOpenIcon />
 			View
 		</Button>
-		<Button className="flex items-center gap-2">
+		<Button className="flex items-center gap-2" variant="outline">
 			<Pencil1Icon />
 			Edit
 		</Button>
-		<Button className="flex items-center gap-2 text-rose-500">
+		<Button className="flex items-center gap-2 text-rose-500" variant='outline'>
 			<TrashIcon />
 			Delete
 		</Button>
--- lib/actions/blog.ts
+++ lib/actions/blog.ts
@@ -39,3 +39,10 @@ export async function createBlog(data: BlogFormSchemaType) {
 		return JSON.stringify(result);
 	}
 }
+
+export async function readBlog() {
+	return supabase
+		.from("blog")
+		.select("*")
+		.order("created_at", { ascending: true });
+}
